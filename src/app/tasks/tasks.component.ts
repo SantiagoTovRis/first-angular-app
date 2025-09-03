aa0db8a35ec7234af3517f829ddbb436f93ba854
SantiagoTovRis <santiago3tovar@gmail.com>
2025-09-03 18:32:42 -0500

    new task window functionality created

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component'
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent,NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -11,6 +12,9 @@ import { TaskComponent } from './task/task.component';
 export class TasksComponent {
   @Input({required: true}) name!: string;
   @Input({required: true}) userId!: string;
+
+  isAddingTasks = false;
+
   tasks = [
     {
       id: 't1',
@@ -43,5 +47,13 @@ export class TasksComponent {
 
   onCompleteTask(id:string){
     this.tasks = this.tasks.filter((task)=> task.id !== id );
+  }
+
+  onStartAddTask(){
+    this.isAddingTasks = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTasks = false;
   }
 }
